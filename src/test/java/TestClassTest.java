29e9619406a170f2d2c33061d26f368af4e2fa60
zagora17 <zagora171991@gmail.com>
2021-10-23 15:17:52 +0200

    add test class and dependency in maven

---
+++ src/test/java/TestClassTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestClassTest {
+    @Test
+    public void shouldDisplayMessageInConsole() {
+        assertEquals(1, 1);
+    }
+}
